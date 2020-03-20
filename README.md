cc32b700ef73d53ab4cc412cf63c8dca7111b693
Chris Pan <chrispan.pt@gmail.com>
2020-03-20 12:38:53 +0100

    :zap:

---
+++ README.md
@@ -0,0 +1,8 @@
+# Skeleton Portfolio
+
+A portfolio page built using Skeleton. See [Live Preview][1] here.
+
+![Screenshot][2]
+
+[1]: https://panteng.github.io/skeleton-portfolio/
+[2]: https://raw.githubusercontent.com/panteng/skeleton-portfolio/master/screenshot.png
---
+++ config/vendors.js
@@ -0,0 +1,18 @@
+module.exports = {
+    stylesheets: [
+        'bower_components/normalize-css/normalize.css',
+        'bower_components/skeleton/css/skeleton.css',
+        'bower_components/font-awesome/css/font-awesome.css'
+    ],
+
+    javascripts: [
+    ],
+
+    fonts: [
+        'bower_components/font-awesome/fonts/*'
+    ],
+
+    standalone_javascripts: [
+        'bower_components/html5shiv/dist/html5shiv.js'
+    ]
+};
